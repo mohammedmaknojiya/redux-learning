85dc6fc0ec7fcaadf521ea440fa0ffb4b4cf77d6
mohammedmaknojiya <mmra9593@gmail.com>
2022-07-01 17:23:10 +0530

    Replcae custom thunk with redux thunk middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
-import customThunkMiddleware from "../customMiddleware/CustomThunkMiddleware";
+// import customThunkMiddleware from "../customMiddleware/CustomThunkMiddleware";
+import thunk from "redux-thunk";
 
-let store = createStore(rootReducer, applyMiddleware(customThunkMiddleware));
+// let store = createStore(rootReducer, applyMiddleware(customThunkMiddleware));
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
